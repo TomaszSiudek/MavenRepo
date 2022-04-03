9d8e27e2ec84cbc111b2764acbf3c7effada9a5f
Tomasz Siudek <tomaszs1990@gmail.com>
2022-04-03 13:53:45 +0200

    pierwszy commit

---
+++ src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,12 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    // Generuje metodę startową:
+    // psvm - public static void main
+    public static void main(String[] args) {
+        // Generuje komendę wypisania do konsoli:
+        // sout -> System.out.println();
+        // użyj systemowego wyjścia, wypisz linię:
+        System.out.println("Hello World!");
+    }
+}
